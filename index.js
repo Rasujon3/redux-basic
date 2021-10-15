25569c1bf37c4f4d55b336e284a728d8c042b816
Ruhul Amin Sujon <rasujon3@gmail.com>
2021-10-16 01:43:50 +0600

    Dispatch action

--- index.js
+++ index.js
@@ -9,9 +9,33 @@ const initState = {
 
 // Reducer
 const rootReducer = (state = initState, action) => {
+    if (action.type === 'INC_NUM') {
+        return {
+            ...state,
+            num: state.num + 1
+        }
+    }
+    if (action.type === 'ADD_NUM') {
+        return {
+            ...state,
+            num: state.num + action.value
+        }
+    }
     return state;
 }
 
 // store
 const store = redux.createStore(rootReducer);
+console.log(store.getState());
+
+// Dispatching Action
+store.dispatch({
+    type: 'INC_NUM'
+});
+
+store.dispatch({
+    type: 'ADD_NUM',
+    value: 34
+});
+
 console.log(store.getState());
